f718c1ae14e59b3e63dcbc45ab90760f04d54b37
MehrajLatifli <meracletifli@gmail.com>
2024-06-22 01:35:53 +0400

    GOD bless universes

--- app/src/main/java/com/example/androidlesson24/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/androidlesson24/views/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidlesson24.views.activities
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        enableEdgeToEdge()
+
+
 
     }
 }
